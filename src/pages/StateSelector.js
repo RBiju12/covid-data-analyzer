2f197b94662bf4c408298009742c2a76352532f0
Rishan Biju <rishanbiju@gmail.com>
2023-02-28 00:21:23 -0500

    Added ApexChart need to filter to rerender page

--- src/pages/StateSelector.js
+++ src/pages/StateSelector.js
@@ -1,9 +1,61 @@
 import React from 'react'
 import states from './states.json'
 import PropTypes  from 'prop-types';
+import ReactApexChart from 'react-apexcharts';
 
 class StateSelector extends React.Component{
+
     
+    constructor(props){
+        super(props);
+
+        this.state = {
+            options: {
+            chart: {
+                id: 'line-chart'
+            },
+            xaxis: {
+                categories: []
+            }
+            },
+            series: [{
+                name: 'series1',
+                data: []
+            }]
+        };
+
+        }
+
+
+    fetchData = () => {
+        fetch("https://api.covidtracking.com/v1/states/current.json")
+        .then(response=> response.json())
+        .then(data => {
+            const dates = data.map(item => item.date);
+            const cases = data.map(item => item.probableCases);
+       
+            this.setState({
+                options: {
+                    ...this.state.options,
+                    xaxis: {
+                        categories: categories
+                    }
+                },
+                series: [{
+                    name: 'series1',
+                    data: seriesData
+                }]
+            })
+        })
+    }
+
+
+    ComponentMountCheck = () => {
+        this.fetchData()
+    }
+
+
+
     handleChange = event => {
         const {onChange} = this.props;
         onChange(event.target.value);
@@ -11,14 +63,20 @@ class StateSelector extends React.Component{
     render(){
         const{id, className} = this.props
     return(
-        <select id={id} className={className} onChange={this.handleChange} >
-        {states.map(item => (
-            <option key={item.abbreviation} value={item.abbreviation}>
-            {item.name}
+        <>
+        <select id={id} className={className} onChange={this.handleChange}>
+            {states.map(item => (
 
-            </option>
-        ))}
+                <option key={item.abbreviation} value={item.abbreviation}>
+                    {item.name}
+
+                </option>
+            ))}
         </select>
+        <div className="chart1">
+                <ReactApexChart options={this.state.options} series={this.state.series} type="line" height={200} />
+            </div>
+            </>
     )
         }
 
