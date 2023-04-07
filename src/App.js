662960702c579aab376d2b210c3a8eb9e18c6afc
Rishan Biju <rishanbiju@gmail.com>
2023-04-06 22:05:42 -0400

    Added starter page and changed Logo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
-import React from 'react';
-import "./stylesheet.css"
-import Footer from "./Footer"
-import Navbar from "./Navbar";
+import React, { useState } from 'react';
+import './stylesheet.css';
+import Footer from './Footer';
+import Navbar from './Navbar';
 import About from './pages/About';
 import DataVisualizations from './pages/DataVisualizations';
-import Trends from './pages/Trends'
+import Trends from './pages/Trends';
 import News from './pages/News';
 import Home from './pages/Home';
-import {Route, Routes} from 'react-router-dom'
-function App(){
-    
-    return(
-        <>
-        <Navbar />
-        <div className='container'>  
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/DataVisualizations" element={<DataVisualizations />} />
-                <Route path="/Trends" element={<Trends />} />
-                <Route path="/News" element={<News />} />
-                <Route path="/About" element={<About />} />
+import { Route, Routes } from 'react-router-dom';
 
-            </Routes>
-        </div>
-        <Footer />
-        </>
-    );
+function Popup({ onClose }) {
+  return (
+    <div className='popup'>
+      <h2>Welcome to my website!</h2>
+      <p><strong>Stay Informed on COVID</strong></p>
+      <button className='close-btn' onClick={onClose}>Close</button>
+    </div>
+  );
+}
+
+function App() {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <>
+      {showPopup && <Popup onClose={handleClosePopup} />}
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/DataVisualizations' element={<DataVisualizations />} />
+          <Route path='/Trends' element={<Trends />} />
+          <Route path='/News' element={<News />} />
+          <Route path='/About' element={<About />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
